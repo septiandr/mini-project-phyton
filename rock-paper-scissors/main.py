27cde24a46d30a8bb30d89b5486ded9c9c024d7e
“septiandr” <sdwirisanggalih@gmail.com>
2025-05-29 19:54:29 +0700

    define variable

---
+++ rock-paper-scissors/main.py
@@ -0,0 +1,8 @@
+select = ['rock', 'paper', 'scissors']
+
+print("------- Rock Paper Scissors Game ---------")
+print("Select one of the following: rock, paper, scissors")
+print("-------------------------------------------")
+
+for i, item in enumerate(select) :
+    print(f"{i}: {item}")
