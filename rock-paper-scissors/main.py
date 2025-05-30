3765385c9afebbdbfe70384f1f0d7afb2dfb13af
“septiandr” <sdwirisanggalih@gmail.com>
2025-05-30 07:10:03 +0700

    add rock scissord

---
+++ rock-paper-scissors/main.py
@@ -0,0 +1,31 @@
+from pickle import NONE
+import random
+
+
+angka = NONE
+
+def selectAngka():
+    return random.randint(1, 100)
+
+def inputAngka():
+    while True:
+        userAngka = int(input("Tebak Angka 1 - 100 : "))
+        if 1 <= userAngka <= 100:
+            return userAngka
+        else:
+            print('Angka tidak valid, solahkan coba lagi')
+
+def cekAngka(angkaComputer):
+    while True:
+        userAngka = inputAngka()
+        if angkaComputer == userAngka :
+            print(f"Angka Benar, : {angkaComputer}")
+            break
+        elif angkaComputer > userAngka :
+            print("Angka terlalu kecil")
+        else:
+            print("Angka terlalu besar")
+
+angkaComputer = selectAngka()
+
+cekAngka(angkaComputer)
