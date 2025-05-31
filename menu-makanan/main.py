53bbdaa88af8c4253bc692a4eca7e989e3a2461c
“septiandr” <sdwirisanggalih@gmail.com>
2025-06-01 06:05:13 +0700

    fix pilihan

--- menu-makanan/main.py
+++ menu-makanan/main.py
@@ -12,22 +12,31 @@ def print_menu(menu):
         for i, (nama, harga) in enumerate(daftar_item.items(), start=1):  # Loop item dalam kategori
             print(f"  {i}. {nama} : Rp{harga:,}")  # Cetak nama dan harga
 
-def input_menu():
+def input_menu(key, menu):
     while True:
-        print("Masukkan Pilihan Makanan Anda (masukkan dalam nomor):")
+        print(f"Masukkan Pilihan {key} Anda (masukkan dalam nomor):")
         makanan = int(input())
         index = makanan - 1
-        listMakanan = list(menu['makanan'].items())
-        print(listMakanan)
+        listMakanan = list(menu.items())
         newMakanan = listMakanan[index]
-        print(newMakanan)
-        break
-        # pilihan_user.append(menu('makanan[index]'))
+        pilihan_user.append(newMakanan)
+
+        if pilihan(key) == False:
+            break
 
 
+def pilihan(key):
+    is_order = input(f'apakah anda ingin memesan {key} lagi? y/n: ')
+    is_order = is_order.lower()
+    if is_order == 'y':
+        return True  # Lanjutkan loop input_menu
+    else:
+        print('terimakasih sudah memesan')
+        return False  # Hentikan loop input_menu
 
-    
 
 
 print_menu(menu)
-input_menu()
+input_menu('Makanan', menu['makanan'])
+input_menu('Minuman', menu['minuman'])
+# pilihan_user()
