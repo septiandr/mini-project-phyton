0eae546b4b3f354b4513890ea73490e4d2b7ca4c
“septiandr” <sdwirisanggalih@gmail.com>
2025-06-01 05:54:22 +0700

    select menu single

--- menu-makanan/main.py
+++ menu-makanan/main.py
@@ -3,6 +3,8 @@ menu = {
     'minuman': {'es teh': 3000, 'es jeruk': 40000, 'kopi': 5000}
 }
 
+pilihan_user = []
+
 def print_menu(menu):
     print("===== DAFTAR MENU =====")  # Header menu
     for kategori, daftar_item in menu.items():  # Loop tiap kategori: 'makanan', 'minuman'
@@ -10,6 +12,22 @@ def print_menu(menu):
         for i, (nama, harga) in enumerate(daftar_item.items(), start=1):  # Loop item dalam kategori
             print(f"  {i}. {nama} : Rp{harga:,}")  # Cetak nama dan harga
 
+def input_menu():
+    while True:
+        print("Masukkan Pilihan Makanan Anda (masukkan dalam nomor):")
+        makanan = int(input())
+        index = makanan - 1
+        listMakanan = list(menu['makanan'].items())
+        print(listMakanan)
+        newMakanan = listMakanan[index]
+        print(newMakanan)
+        break
+        # pilihan_user.append(menu('makanan[index]'))
+
+
+
+    
 
 
 print_menu(menu)
+input_menu()
