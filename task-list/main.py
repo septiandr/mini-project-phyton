5005c33c3ac141ad404f7a49492fd21d741a624d
“septiandr” <sdwirisanggalih@gmail.com>
2025-06-02 09:02:32 +0700

    def list task

--- task-list/main.py
+++ task-list/main.py
@@ -9,3 +9,22 @@ print("Menu")
 print("1. Create Task")
 print("2. Delete Task")
 print("3. Show Task")
+
+
+task = []
+def create_task():
+    
+    task_name = input("Input Task Name: ")
+
+    if task_name in task:
+        print("Task Already Exist")
+        return
+    else:
+        task.append(task_name)
+        print("Task Created")
+
+def list_task():
+    for i, item in enumeration(task, start=1):
+        print(f"{i}. {item}")
+
+
