ac61fd997b32be4fadb627dcffb028fcde7e0c72
“septiandr” <sdwirisanggalih@gmail.com>
2025-06-02 09:10:11 +0700

    finish

--- task-list/main.py
+++ task-list/main.py
@@ -1,17 +1,27 @@
-print("Task List")
-print("Program build by: gav")
-print("-------------------")
-
-print("Program built to handle create delete and show task list")
-
-print("-------------------")
-print("Menu")
-print("1. Create Task")
-print("2. Delete Task")
-print("3. Show Task")
-
 
 task = []
+def show_menu():
+    print("Task List")
+    print("Program build by: gav")
+    print("-------------------")
+
+    print("Program built to handle create delete and show task list")
+    print("-------------------")
+    print("Menu")
+    print("1. Create Task")
+    print("2. Delete Task")
+    print("3. Show Task")
+
+    menu = input("Input Menu: ")
+    if menu == "1":
+        create_task()
+    elif menu == "2":
+        delete_task()
+    elif menu == "3":
+        list_task()
+    else:
+        print("Menu Not Found")
+
 def create_task():
     
     task_name = input("Input Task Name: ")
@@ -33,3 +43,13 @@ def delete_task():
     index_data_task = int(delete_task_number) - 1
     task.pop(index_data_task)
     print("Task Deleted")
+
+def back_to_menu():
+    print("-------------------")
+    back_menu = int(input("Press 0 to Back to Menu : "))
+    if back_menu == 0 :
+        show_menu()
+    else:
+        print("Menu Not Found")
+
+show_menu()
