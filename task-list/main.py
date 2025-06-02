0dab6d440487be02abf5a61bb78f83855cb3a971
“septiandr” <sdwirisanggalih@gmail.com>
2025-06-02 08:57:21 +0700

    create task def

---
+++ task-list/main.py
@@ -0,0 +1,11 @@
+print("Task List")
+print("Program build by: gav")
+print("-------------------")
+
+print("Program built to handle create delete and show task list")
+
+print("-------------------")
+print("Menu")
+print("1. Create Task")
+print("2. Delete Task")
+print("3. Show Task")
