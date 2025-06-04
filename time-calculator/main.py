175db4f6473e39978c26204d732aeac8ba0eed61
“septiandr” <sdwirisanggalih@gmail.com>
2025-06-04 12:45:22 +0700

    menu

--- time-calculator/main.py
+++ time-calculator/main.py
@@ -11,6 +11,21 @@ def start():
     print(f"Time: {now.strftime('%H:%M:%S')}")
     print(f"\nTimezone: {timezone}")
     print("\n=============================")
+    print("\n")
+    print("Menu:")
+    print("1. Add Time")
+    print("2. Subtract Time")
+    print("3. Exit\n")
+    menu = input("Pilih menu: ")
+    if menu == "1":
+        add_time()
+    elif menu == "2":
+        subtract_time()
+    elif menu == "3":
+        exit()
+    else:
+        print("Pilihan tidak tersedia")
+        start()
 
 
 
