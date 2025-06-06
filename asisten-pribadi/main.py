f6abbcc201c3eb9216559f6ee92ffceb5c6da44d
“septiandr” <sdwirisanggalih@gmail.com>
2025-06-06 22:47:18 +0700

    start

---
+++ asisten-pribadi/main.py
@@ -0,0 +1,23 @@
+def main():
+    print("Hi Gav")
+    print("What do you want to do?\n")
+
+    print("1. Add Note")
+    print("2. Show Notes")
+    print("3. Calculator")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        add_note()
+    elif choice == "2":
+        show_notes()
+    elif choice == "2":
+        show_notes()
+    elif choice == "4":
+        exit()
+    else:
+        print("Invalid choice")
+        main()
+
+main()
